3891c94bfbf263bcba9c1caacdb3f28ecb156e55
Anthony Kim <anthonymoosungkim@proton.me>
2024-11-11 16:48:22 -0500

    Helped by Eric Donnelly

--- exercises/e5.js
+++ exercises/e5.js
@@ -24,7 +24,7 @@ export const attachTitle = (arg) => {
 export const getPromise = () => {
   // Your code goes here...
   return Promise.resolve('MANHATTAN')
-    .then(attachTitle)
+    .then(val => attachTitle(val))
     .then(result => {
       console.log(result);
       return result;
